6a45740f895b2eaaa1ea090b0560a0ceae6568a0
Verde <sl71951206@idat.edu.pe>
2024-02-21 17:02:09 -0500

    Guardado para la EF

--- sm_spa.sql
+++ sm_spa.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 30-01-2024 a las 22:56:16
--- Versión del servidor: 10.4.24-MariaDB
--- Versión de PHP: 8.1.6
+-- Tiempo de generación: 21-02-2024 a las 18:43:45
+-- Versión del servidor: 10.4.28-MariaDB
+-- Versión de PHP: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,49 +24,45 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `empleado`
+-- Estructura de tabla para la tabla `empleados`
 --
 
-CREATE TABLE `empleado` (
+CREATE TABLE `empleados` (
   `id_empleado` int(11) NOT NULL,
-  `nombre` varchar(100) DEFAULT NULL,
-  `especialidad_id` int(11) DEFAULT NULL,
-  `horario_trabajo` varchar(100) DEFAULT NULL,
-  `url_foto` varchar(500) DEFAULT NULL,
-  `estado` tinyint(1) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Volcado de datos para la tabla `empleado`
---
-
-INSERT INTO `empleado` (`id_empleado`, `nombre`, `especialidad_id`, `horario_trabajo`, `url_foto`, `estado`) VALUES
-(1, 'Luisa Fernández', 1, '9:30 - 18:00', 'https://media.gettyimages.com/id/1126310650/es/foto/retrato-de-un-due%C3%B1o-de-negocio-trabajando-en-un-spa.jpg?s=612x612&w=0&k=20&c=pGita07Su5u4H1wtNUXGCl01TK6k69fpEFQNnRajS44=', 1),
-(2, 'Valeria Quintana', 1, '9:30 - 18:00', 'https://media.gettyimages.com/id/1329039746/es/foto/recepcionista-feliz-trabajando-en-una-peluquer%C3%ADa-en-la-recepci%C3%B3n.jpg?s=612x612&w=0&k=20&c=NYJgnv_H3MeU4KekeWHoqUO2lic6SA4NWLc-E59xJjM=', 1),
-(3, 'Martha Cardenas', 2, '9:30 - 18:00', 'https://media.gettyimages.com/id/1296150604/es/foto/terapeuta-de-belleza-aprendiz.jpg?s=612x612&w=0&k=20&c=Wrne-Xp0EYYZd5a2tvRi5PzS_ycEAvYO8uiGJ8IvVls=', 1),
-(4, 'Luis Díaz', 2, '9:30 - 18:00', 'https://media.gettyimages.com/id/1397286352/es/foto/business-manager-verificando-reservas-en-tableta-digital.jpg?s=612x612&w=0&k=20&c=DrqDPhZjSkN78a9nBh4K1dUmun6O-WDGTueHNAsHM2A=', 1),
-(5, 'Josefina Linares', 3, '9:30 - 18:00', 'https://media.gettyimages.com/id/904991460/es/foto/smiling-woman-waiting-for-customers-debica-poland.jpg?s=612x612&w=0&k=20&c=h74utqAYnG5Wsywnc_mlNxP7LU93iIyAFTACaxfdGWU=', 1),
-(6, 'Lizbeth Suárez', 3, '9:30 - 18:00', 'https://media.gettyimages.com/id/1029003016/es/foto/hermosa-empresaria-de-un-pie-de-sal%C3%B3n-de-pelo-detr%C3%A1s-de-la-recepci%C3%B3n-sonriendo-a-c%C3%A1mara.jpg?s=612x612&w=0&k=20&c=SDupUwGxkP9WMClk9wBjMr5iWxNa2-99McJ9FEs_-fc=', 1);
+  `apellidos` varchar(200) NOT NULL,
+  `correo` varchar(300) NOT NULL,
+  `descripcion` varchar(2000) DEFAULT NULL,
+  `estado` bit(1) NOT NULL,
+  `nombres` varchar(200) NOT NULL,
+  `telefono` varchar(50) NOT NULL,
+  `url_foto` varchar(1000) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `instalacion`
+-- Estructura de tabla para la tabla `instalaciones`
 --
 
-CREATE TABLE `instalacion` (
+CREATE TABLE `instalaciones` (
   `id_instalacion` int(11) NOT NULL,
-  `nombre` varchar(100) DEFAULT NULL,
-  `descripcion` text DEFAULT NULL,
-  `estado` varchar(20) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `color` char(7) DEFAULT NULL,
+  `descripcion` varchar(2000) DEFAULT NULL,
+  `estado` bit(1) NOT NULL,
+  `rotulo` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
 
 --
--- Volcado de datos para la tabla `instalacion`
+-- Estructura de tabla para la tabla `perfiles`
 --
 
-INSERT INTO `instalacion` (`id_instalacion`, `nombre`, `descripcion`, `estado`) VALUES
-(1, 'BAÑO', 'TUBAÑO', 'Activo');
+CREATE TABLE `perfiles` (
+  `id_perfil` int(11) NOT NULL,
+  `contrasena` varchar(255) NOT NULL,
+  `usuario` enum('USER','ADMIN') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -76,25 +72,15 @@ INSERT INTO `instalacion` (`id_instalacion`, `nombre`, `descripcion`, `estado`) 
 
 CREATE TABLE `promociones` (
   `id_promocion` int(11) NOT NULL,
-  `titulo` varchar(100) DEFAULT NULL,
-  `descripcion` varchar(255) DEFAULT NULL,
-  `estado` tinyint(1) DEFAULT NULL,
-  `fecha_inicio` date DEFAULT NULL,
-  `fecha_fin` date DEFAULT NULL,
-  `url_imagen_promocion` varchar(500) DEFAULT NULL,
-  `descuento` int(11) DEFAULT NULL,
-  `tipo` varchar(50) DEFAULT NULL,
-  `servicio_id` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Volcado de datos para la tabla `promociones`
---
-
-INSERT INTO `promociones` (`id_promocion`, `titulo`, `descripcion`, `estado`, `fecha_inicio`, `fecha_fin`, `url_imagen_promocion`, `descuento`, `tipo`, `servicio_id`) VALUES
-(1, 'El Tercer Masaje con 50% descuento', 'Después de haber reservado 2 veces el Servicio de Masajes , la tercera reserva tiene un descuento del 50%. Revise las condiciones en la imagen mostrada.', 1, '2024-01-11', '2024-01-27', 'SASDASDASDASD', 50, '1', 2),
-(2, 'El Tercer Baño Termal con 50% descuento ', 'Después de haber reservado 2 veces el Servicio de Baño Termal, la tercera reserva tiene un descuento del 50%. Revise las condiciones en la imagen mostrada.', 1, '2024-01-02', '2024-01-10', 'assadasdasd', 50, '1', 7),
-(3, 'PROMO 3', 'NUEVA DESC', 0, '2024-01-02', '2024-01-10', 'assadasdasd', 50, 'asdsda', 1);
+  `descripcion` varchar(3000) DEFAULT NULL,
+  `descuento` smallint(6) NOT NULL,
+  `estado` bit(1) NOT NULL,
+  `fecha_fin` date NOT NULL,
+  `fecha_inicio` date NOT NULL,
+  `tipo` enum('EFECTIVO','PORCENTUAL') NOT NULL,
+  `titulo` varchar(500) NOT NULL,
+  `url_imagen` varchar(1000) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -104,154 +90,147 @@ INSERT INTO `promociones` (`id_promocion`, `titulo`, `descripcion`, `estado`, `f
 
 CREATE TABLE `reservas` (
   `id_reserva` int(11) NOT NULL,
-  `servicio` int(11) DEFAULT NULL,
-  `fecha` date DEFAULT NULL,
-  `hora` time DEFAULT NULL,
-  `correo_cliente` varchar(100) DEFAULT NULL,
-  `nombre_cliente` varchar(50) DEFAULT NULL,
-  `apellido_cliente` varchar(50) DEFAULT NULL,
-  `telefono_cliente` varchar(15) DEFAULT NULL,
-  `empleado` int(11) DEFAULT NULL,
-  `instalacion` int(11) DEFAULT NULL,
-  `promocion` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Volcado de datos para la tabla `reservas`
---
-
-INSERT INTO `reservas` (`id_reserva`, `servicio`, `fecha`, `hora`, `correo_cliente`, `nombre_cliente`, `apellido_cliente`, `telefono_cliente`, `empleado`, `instalacion`, `promocion`) VALUES
-(1, 1, '2024-01-03', '13:23:01', 'DASDASD@GMAIL.COM', 'LUCHO', 'PEDRAZA', '94613817', 1, 1, 1);
+  `apellidos_cliente` varchar(200) NOT NULL,
+  `correo_cliente` varchar(300) NOT NULL,
+  `estado` enum('CANCELADO','IMPAGO','PAGADO','REPROGRAMADO') NOT NULL,
+  `fecha` date NOT NULL,
+  `hora` time NOT NULL,
+  `hora_fin` time NOT NULL,
+  `nombres_cliente` varchar(200) NOT NULL,
+  `telefono_cliente` varchar(50) NOT NULL,
+  `id_empleado` int(11) NOT NULL,
+  `id_instalacion` int(11) NOT NULL,
+  `id_promocion` int(11) DEFAULT NULL,
+  `id_servicio` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `servicio`
+-- Estructura de tabla para la tabla `servicios`
 --
 
-CREATE TABLE `servicio` (
+CREATE TABLE `servicios` (
   `id_servicio` int(11) NOT NULL,
-  `nombre` varchar(100) DEFAULT NULL,
-  `descripcion` varchar(500) DEFAULT NULL,
-  `duracion` int(11) DEFAULT NULL,
-  `precio` decimal(10,2) DEFAULT NULL,
-  `url_imagen` varchar(500) DEFAULT NULL,
-  `categoria` varchar(50) DEFAULT NULL,
-  `favorito` tinyint(1) NOT NULL,
-  `estado` tinyint(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Volcado de datos para la tabla `servicio`
---
-
-INSERT INTO `servicio` (`id_servicio`, `nombre`, `descripcion`, `duracion`, `precio`, `url_imagen`, `categoria`, `favorito`, `estado`) VALUES
-(1, 'Tratamiento Facial Hidratante', 'Hidratación profunda para tu rostro. Nuestro tratamiento facial hidratante deja la piel suave y luminosa.', 45, '75.00', 'https://i0.wp.com/qpwebsite.s3.amazonaws.com/uploads/2017/10/b5_faciales.jpg?fit=1200%2C800&ssl=1', '1', 1, 1),
-(2, 'Masaje Relajante', 'Experimenta la paz y relajación profunda con nuestro masaje relajante. Nuestros terapeutas expertos te brindarán una experiencia rejuvenecedora.', 60, '80.00', 'https://www.trainingforgold.es/images/easyblog_articles/23/masajes-relajacion-sevilla-fisioterapeuta.jpg', '1', 1, 1),
-(3, 'Limpieza Facial Profunda', 'Renueva tu piel con nuestro facial de limpieza profunda. Elimina impurezas y revitaliza tu cutis para una apariencia fresca y saludable.', 45, '100.00', 'https://i0.wp.com/menshealthlatam.com/wp-content/uploads/2023/12/mejor-limpieza-facial.jpg?fit=1200%2C800&ssl=1', '1', 1, 1),
-(4, 'Tratamiento Capilar Hidratación', 'Devuelve la vitalidad a tu cabello con nuestro tratamiento capilar de hidratación intensa. Cabello suave, sedoso y lleno de vida.', 30, '70.00', 'https://piellem.com/assets/imagens/Hair-Schedule1200800px.jpg', '2', 0, 1),
-(5, 'Exfoliación Corporal Aromaterapia', 'Disfruta de una exfoliación corporal que combina la aromaterapia para una experiencia sensorial única. Deja tu piel suave y radiante.', 40, '90.00', 'https://graziamagazine.com/es/wp-content/uploads/sites/12/2022/05/todo-lo-que-debes-saber-sobre-la-exfoliacion-corporal.jpg?fit=1200%2C800', '2', 0, 1),
-(6, 'Manicura y Pedicura Hecho en Gel', 'Embellece tus manos y pies con nuestra manicura y pedicura spa. Un cuidado completo para uñas y cutículas.', 60, '50.00', 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fnoticias.tendenzias.com%2Ftrendy%2Funas-gel&psig=AOvVaw1_d_uZF0KJvBi65yequfyL&ust=1706482869887000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCODjrbzW_oMDFQAAAAAdAAAAABB3', '3', 0, 0),
-(7, 'Baños Termales Rejuvenecedores', 'Sumérgete en la relajación total con nuestros baños termales. Una experiencia rejuvenecedora para su cuerpo, pero también para la mente.', 120, '120.00', 'https://estaticos-cdn.prensaiberica.es/clip/72082620-f68b-46bb-aa25-cc18a8c70fc2_woman-libre-1200_default_0.jpg', '4', 1, 1);
+  `categoria` varchar(100) NOT NULL,
+  `descripcion` varchar(2000) DEFAULT NULL,
+  `duracion` smallint(6) NOT NULL,
+  `estado` bit(1) NOT NULL,
+  `favorito` bit(1) NOT NULL,
+  `nombre` varchar(200) NOT NULL,
+  `precio` decimal(7,2) NOT NULL,
+  `url_imagen` varchar(1000) DEFAULT NULL,
+  `id_promocion` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Índices para tablas volcadas
 --
 
 --
--- Indices de la tabla `empleado`
+-- Indices de la tabla `empleados`
 --
-ALTER TABLE `empleado`
+ALTER TABLE `empleados`
   ADD PRIMARY KEY (`id_empleado`),
-  ADD KEY `especialidad_id` (`especialidad_id`);
+  ADD UNIQUE KEY `UK_h1yoynfig4dn2d3vff3luwyj` (`correo`),
+  ADD UNIQUE KEY `UK_iik9xxjtgmft5hfuh7nx4whcp` (`telefono`);
 
 --
--- Indices de la tabla `instalacion`
+-- Indices de la tabla `instalaciones`
 --
-ALTER TABLE `instalacion`
-  ADD PRIMARY KEY (`id_instalacion`);
+ALTER TABLE `instalaciones`
+  ADD PRIMARY KEY (`id_instalacion`),
+  ADD UNIQUE KEY `UK_fdpr7lqdqo4xpa4clror4rpq3` (`rotulo`);
+
+--
+-- Indices de la tabla `perfiles`
+--
+ALTER TABLE `perfiles`
+  ADD PRIMARY KEY (`id_perfil`);
 
 --
 -- Indices de la tabla `promociones`
 --
 ALTER TABLE `promociones`
   ADD PRIMARY KEY (`id_promocion`),
-  ADD KEY `servicio_id` (`servicio_id`);
+  ADD UNIQUE KEY `UK_lljj6r5d6itrrbp6k6d8skvhe` (`titulo`);
 
 --
 -- Indices de la tabla `reservas`
 --
 ALTER TABLE `reservas`
   ADD PRIMARY KEY (`id_reserva`),
-  ADD KEY `servicio` (`servicio`),
-  ADD KEY `empleado` (`empleado`),
-  ADD KEY `instalacion` (`instalacion`),
-  ADD KEY `promocion` (`promocion`);
+  ADD KEY `FKol1of3eey7mg2dww0dfpj13bx` (`id_empleado`),
+  ADD KEY `FKe8ssyctibtie036e8eu696tc6` (`id_instalacion`),
+  ADD KEY `FKruk66ryywymrkhyyni9xddi3w` (`id_promocion`),
+  ADD KEY `FK2owpd8tl1b57k4cmqhuttlu2e` (`id_servicio`);
 
 --
--- Indices de la tabla `servicio`
+-- Indices de la tabla `servicios`
 --
-ALTER TABLE `servicio`
-  ADD PRIMARY KEY (`id_servicio`);
+ALTER TABLE `servicios`
+  ADD PRIMARY KEY (`id_servicio`),
+  ADD UNIQUE KEY `UK_lu7evqbgflb102gqcmbg8y8mf` (`nombre`),
+  ADD KEY `FK4mgseh68bpfpavtco8pciuug5` (`id_promocion`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
 --
--- AUTO_INCREMENT de la tabla `empleado`
+-- AUTO_INCREMENT de la tabla `empleados`
 --
-ALTER TABLE `empleado`
-  MODIFY `id_empleado` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+ALTER TABLE `empleados`
+  MODIFY `id_empleado` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT de la tabla `instalacion`
+-- AUTO_INCREMENT de la tabla `instalaciones`
 --
-ALTER TABLE `instalacion`
-  MODIFY `id_instalacion` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+ALTER TABLE `instalaciones`
+  MODIFY `id_instalacion` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `perfiles`
+--
+ALTER TABLE `perfiles`
+  MODIFY `id_perfil` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `promociones`
 --
 ALTER TABLE `promociones`
-  MODIFY `id_promocion` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_promocion` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `reservas`
 --
 ALTER TABLE `reservas`
-  MODIFY `id_reserva` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_reserva` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT de la tabla `servicio`
+-- AUTO_INCREMENT de la tabla `servicios`
 --
-ALTER TABLE `servicio`
-  MODIFY `id_servicio` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+ALTER TABLE `servicios`
+  MODIFY `id_servicio` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- Restricciones para tablas volcadas
 --
 
 --
--- Filtros para la tabla `empleado`
---
-ALTER TABLE `empleado`
-  ADD CONSTRAINT `empleado_ibfk_1` FOREIGN KEY (`especialidad_id`) REFERENCES `servicio` (`id_servicio`);
-
---
--- Filtros para la tabla `promociones`
---
-ALTER TABLE `promociones`
-  ADD CONSTRAINT `promociones_ibfk_1` FOREIGN KEY (`servicio_id`) REFERENCES `servicio` (`id_servicio`);
-
---
 -- Filtros para la tabla `reservas`
 --
 ALTER TABLE `reservas`
-  ADD CONSTRAINT `reservas_ibfk_1` FOREIGN KEY (`servicio`) REFERENCES `servicio` (`id_servicio`),
-  ADD CONSTRAINT `reservas_ibfk_2` FOREIGN KEY (`empleado`) REFERENCES `empleado` (`id_empleado`),
-  ADD CONSTRAINT `reservas_ibfk_3` FOREIGN KEY (`instalacion`) REFERENCES `instalacion` (`id_instalacion`),
-  ADD CONSTRAINT `reservas_ibfk_4` FOREIGN KEY (`promocion`) REFERENCES `promociones` (`id_promocion`);
+  ADD CONSTRAINT `FK2owpd8tl1b57k4cmqhuttlu2e` FOREIGN KEY (`id_servicio`) REFERENCES `servicios` (`id_servicio`),
+  ADD CONSTRAINT `FKe8ssyctibtie036e8eu696tc6` FOREIGN KEY (`id_instalacion`) REFERENCES `instalaciones` (`id_instalacion`),
+  ADD CONSTRAINT `FKol1of3eey7mg2dww0dfpj13bx` FOREIGN KEY (`id_empleado`) REFERENCES `empleados` (`id_empleado`),
+  ADD CONSTRAINT `FKruk66ryywymrkhyyni9xddi3w` FOREIGN KEY (`id_promocion`) REFERENCES `promociones` (`id_promocion`);
+
+--
+-- Filtros para la tabla `servicios`
+--
+ALTER TABLE `servicios`
+  ADD CONSTRAINT `FK4mgseh68bpfpavtco8pciuug5` FOREIGN KEY (`id_promocion`) REFERENCES `promociones` (`id_promocion`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
